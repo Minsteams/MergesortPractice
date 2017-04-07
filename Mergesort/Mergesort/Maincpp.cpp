87970004a9a736b0b3af43aa5a91d903be681d96
Minsteams <474655650@qq.com>
2017-04-07 19:43:15 +0800

    Adding function
    Now you can input your own array and mergesort it.

--- Mergesort/Mergesort/Maincpp.cpp
+++ Mergesort/Mergesort/Maincpp.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include<vector>
 #include "Mergesort.h"
+#include <conio.h>
 using namespace std;
 
 
 void main() {
-	vector<int> a = { 1,2,3,2,4,6,78,5,3,1 };
+	cout << "Please input your arrary with each number separated with space:" << endl;
+	//Input
+	vector<int> a;
+	int t = 0;
+	char b=1;
+	while (b!='\r') {
+		b = _getch();
+		printf_s("%c", b);
+		if (b >= '0'&&b <= '9')t = t * 10 + b - '0';
+		if (b == ' ') {
+			a.push_back(t);
+			t = 0;
+		}
+	}
+	printf_s("\n\n\n");
+	a.push_back(t);
+
+	//mergesort
 	mergeSort(a);
+	//Output
 	for (int i = 0; i < a.size(); i++)
-		cout << a[i] << endl;
-//	getchar();
+		printf("%d ", a[i]);
+	_getch();
 }
